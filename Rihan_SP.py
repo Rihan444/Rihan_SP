9f00aff7b878f7bb63c8d1c84be06f41a7abd5c4
Rihan Ahmed <95731297+Rihan444@users.noreply.github.com>
2022-07-31 14:01:41 +0600

    Add files via upload

---
+++ Rihan_SP.py
@@ -0,0 +1,336 @@
+import os
+import sys
+import webbrowser
+import requests
+from threading import Thread
+import os
+import os
+try:
+	import requests
+except:
+	os.system("pip install requests")
+import requests
+
+import os, sys, time
+from time import sleep as timeout
+def restart_program():
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+        curdir = os.getcwd()
+
+
+# Reset
+color_off="\033[0m"       # Text Reset
+ipurple="\033[0;95m"      # Purple
+ired="\033[0;91m"         # Red
+iyellow="\033[0;93m"      # Yellow
+iblue="\033[0;94m"        # Blue
+icyan="\033[0;96m"        # Cyan
+iwhite="\033[0;97m"       # White
+igreen="\033[0;92m"       # Green
+on_purple="\033[45m"      # Purple
+bgreen="\033[1;32m"       # Green
+
+def ts():
+	os.system("termux-setup-storage")
+	os.system("clear")
+	os.system("pkg install figlet")
+	os.system("clear")
+	os.system("pip install requests")
+	os.system("clear")
+	os.system("pip install futures")
+	os.system("clear")
+	os.system("pkg install python2")
+	os.system("clear")
+	os.system("pkg install python3")
+	os.system("clear")
+	os.system("pip install bs4")
+	os.system("clear")
+	os.system("pip install --upgrade pip")
+	os.system("clear")
+	os.system("pip install bs4")
+	os.system("clear")
+	os.system("pip2 install mechanize")
+	os.system("clear")
+	os.system("pkg Install sl")
+	os.system("clear")
+	os.system("pkg Install cmatrix")
+	os.system("clear")
+	os.system("pkg install sudo")
+	os.system("clear")
+	os.system("clear")
+	os.system("pkg install binutils")
+	os.system("clear")
+	os.system("pkg install binutils-is-llvm")
+	os.system("clear")
+
+def bt():
+	os.system("git clone https://github.com/Bhai4You/Termux-Banner")
+	os.system("clear")
+	os.system("cd Termux-Banner")
+	os.system("clear")
+	os.system("chmod +x requirement.sh")
+	os.system("clear")
+	os.system("chmod +x t-ban.sh")
+	os.system("clear")
+	os.system("bash requirement.sh")
+	os.system("clear")
+	os.system("bash t-ban.sh")
+def ut():
+	os.system("apt update")
+	os.system("clear")
+	os.system("apt upgrade")
+	os.system("clear")
+def bhhc():
+	os.system("xdg-open \'https://facebook.com/groups/3749151271810746\'")
+
+def dpa():
+	os.system("xdg-open \'https://www.facebook.com/white.hat.hacker.Rihan\'")
+def ex():
+	os.system("exit")
+	
+os.system("clear")
+import sys
+import os
+import time
+import socket
+import random
+
+os.system("pkg install sl")
+os.system("sl")
+
+time.sleep(2)
+print ('''\n\n\n\n\n\n\n\n\n
+			
+ __    __      _ _                           
+/ / /\ \ \__ _| | | ___ ___  _ __ ___   ___  
+\ \/  \/ / _` | | |/ __/ _ \| '_ ` _ \ / _ \ 
+ \  /\  / (_| | | | (_| (_) | | | | | |  __/ 
+  \/  \/ \__,_|_|_|\___\___/|_| |_| |_|\___| 
+                                                                                                                                                                               
+''')
+
+os.system("clear")
+
+
+time.sleep(2)
+print('''\n\n\n\n\n\n\n\n\n
+  _____   U  ___ u 
+ |_ " _|   \/"_ \/ 
+   | |     | | | | 
+  /| |\.-,_| |_| | 
+ u |_|U \_)-\___/  
+ _// \\_     \\    
+(__) (__)   (__)   
+''')
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n\n\n\n\n\n 
+  
+  _____   _              
+ |_   _| | |_      ___   
+   | |   | ' \    / -_)  
+  _|_|_  |_||_|   \___|  
+_|"""""|_|"""""|_|"""""| 
+"`-0-0-'"`-0-0-'"`-0-0-' 
+    
+
+''')
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n\n\n\n\n\n 
+____/\\\\\\\\\____________/\\\______________________________________        
+ __/\\\///////\\\_________\/\\\______________________________________       
+  _\/\\\_____\/\\\____/\\\_\/\\\______________________________________      
+   _\/\\\\\\\\\\\/____\///__\/\\\__________/\\\\\\\\\_____/\\/\\\\\\___     
+    _\/\\\//////\\\_____/\\\_\/\\\\\\\\\\__\////////\\\___\/\\\////\\\__    
+     _\/\\\____\//\\\___\/\\\_\/\\\/////\\\___/\\\\\\\\\\__\/\\\__\//\\\_   
+      _\/\\\_____\//\\\__\/\\\_\/\\\___\/\\\__/\\\/////\\\__\/\\\___\/\\\_  
+       _\/\\\______\//\\\_\/\\\_\/\\\___\/\\\_\//\\\\\\\\/\\_\/\\\___\/\\\_ 
+        _\///________\///__\///__\///____\///___\////////\//__\///____\///__
+
+''')
+
+
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n\n\n\n\n\n ___________           .__          
+\__    ___/___   ____ |  |   ______
+  |    | /  _ \ /  _ \|  |  /  ___/
+  |    |(  <_> |  <_> )  |__\___ \ 
+  |____| \____/ \____/|____/____  >
+                                \/  
+                                
+                                ''')
+                                
+                                
+                                
+                                
+os.system("clear")
+
+time.sleep(2)
+print('''                                 
+
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+
+                                     
+''') 
+
+os.system("clear")
+time.sleep(2)
+print('''\n
+
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+
+''')
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+''')
+                                  
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+''')
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n
+
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+''')
+
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n\n
+
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+''')
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n\n\n\
+
+
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+''')
+
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n\n\n\n\
+
+
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+''')
+
+os.system("clear")
+
+time.sleep(2)
+print('''\n\n\n\n\n\n\n\n
+                                     
+ ,-----. ,------. ,------.,--.  ,--. 
+'  .-.  '|  .--. '|  .---'|  ,'.|  | 
+|  | |  ||  '--' ||  `--, |  |' '  | 
+'  '-'  '|  | --' |  `---.|  | `   | 
+ `-----' `--'     `------'`--'  `--' 
+                                     
+''')
+os.system("xdg-open \'https://www.facebook.com/white.hat.hacker.Rihan\'")
+os.system("clear")
+print(icyan+'''
+
+    dMMMMb  dMP dMP dMP .aMMMb  dMMMMb        .dMMMb  dMMMMb 
+   dMP.dMP amr dMP dMP dMP"dMP dMP dMP       dMP" VP dMP.dMP 
+  dMMMMK" dMP dMMMMMP dMMMMMP dMP dMP        VMMMb  dMMMMP"  
+ dMP"AMF dMP dMP dMP dMP dMP dMP dMP       dP .dMP dMP       
+dMP dMP dMP dMP dMP dMP dMP dMP dMP        VMMMP" dMP        
+                                                             
+'''+color_off)
+print(bgreen+'''        	 Devoloped By: 𝙍𝙞𝙝𝙖𝙣 𝘼𝙝𝙢𝙚𝙙            '''                            +color_off)
+
+print(on_purple+'''			Version : 1.0.0'''+color_off)
+
+print('''────┈┈┈┄┄╌╌╌╌┄┄┈┈┈────•────•────────•────•''')
+
+print('''  ''')
+print(iyellow+''' [ 1 ] 𝐓𝐞𝐫𝐦𝐮𝐱 𝐒𝐞𝐭𝐮𝐩  '''+color_off)
+print(iblue+''' [ 2 ] 𝐓𝐞𝐫𝐦𝐮𝐱 𝐁𝐚𝐧𝐧𝐞𝐫  '''+color_off)
+print(icyan+''' [ 3 ] 𝐔𝐩𝐝𝐚𝐭𝐞 𝐓𝐞𝐫𝐦𝐮𝐱 𝐏𝐚𝐜𝐤𝐚𝐠𝐞      '''+color_off)
+print(igreen+''' [ 4 ] 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐬𝐡 𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐇𝐞𝐥𝐩 𝐂𝐞𝐧𝐭𝐞𝐫                    '''+color_off)
+print(''' [ 5 ] 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐚𝐜𝐜𝐨𝐮𝐧𝐭 ''')
+print(ired+''' [ 00 ] 𝐄𝐱𝐢𝐭 '''+color_off)
+print(''' ''')
+
+opt=int(input(ipurple+'''𝑩𝒓𝒖𝒕𝒆 𝑭𝒐𝒓𝒄𝒆 𝑪𝒐𝒏𝒔𝒐𝒍𝒆 ==>>  '''+color_off))
+
+if opt==1:
+	ts()
+elif opt==2:
+	bt() 
+elif opt==3:
+	ut()
+elif opt==4:
+	bhhc()
+elif opt==5:
+	dpa()
+elif opt==00:
+	ex()
+else:
+	print(ired+''' [ x ] Worng Value Enter '''+color_off)
